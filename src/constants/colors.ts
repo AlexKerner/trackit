8a7ba04c286b60c4f5f936b6b73f3af56c75c3af
Alexander <alexanderkerner06@gmail.com>
2026-04-22 17:05:45 -0300

    initial commit

---
+++ src/constants/colors.ts
@@ -0,0 +1,27 @@
+
+
+
+export const colors = {
+  light: {
+    primary: "#2A14B4",
+    bg: "#F6FAFE",
+    surface: "#FFFFFF",
+    font: "#171C1F",
+    fontContrast: "#FFFFFF",
+    variant: "#464554",
+    green: "#10B981",
+    greenContrast: "#00442D",
+    error: "#EF4444"
+  },
+  dark: {
+    primary: "#4338CA",
+    bg: "#0F172A",
+    surface: "#1E293B",
+    font: "#F6FAFE",
+    fontContrast: "#FFFFFF",
+    variant: "#94A3B8",
+    green: "#4EDEA3",
+    greenContrast: "#00442D",
+    error: "#FB7185"
+  }
+}
